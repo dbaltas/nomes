981ee50654b5a3f20a3302556339dc11cc717ef6
Dimitris Baltas <dimitris.baltas@thebeat.co>
2018-04-01 13:18:15 +0300

    config file for xpath rules

--- main.go
+++ main.go
@@ -7,13 +7,15 @@ import (
 	"os"
 
 	"github.com/dbaltas/nomes/parser"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	var inFile string
-	// Load HTML file.
+
 	flag.StringVar(&inFile, "in", "", "the input file in html format")
 	flag.Parse()
+
 	if inFile == "" {
 		log.Print("no in file defined")
 		flag.Usage()
@@ -28,14 +30,18 @@ func main() {
 
 	fmt.Printf("\n\n")
 
-	var configMap = make(map[string]string)
-
-	configMap["main.url"] = "//head/meta[@property='og:url']/@content"
-	configMap["main.results"] = "//div[@id='mainbar']//h2"
-	configMap["main.maxpage"] = "//div[@id='mainbar']//span[@class='page-numbers dots']/following::*/span"
-
-	results := parser.ProcessPatterns(f, configMap)
+	results := parser.ProcessPatterns(f, configMap())
 	for k, v := range results {
 		fmt.Printf("%s: %s\n", k, v)
 	}
+}
+
+func configMap() map[string]string {
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
+
+	return viper.GetStringMapString("mappings")
 }
--- readme.md
+++ readme.md
@@ -1,13 +1,18 @@
+[![Build Status](https://travis-ci.org/dbaltas/nomes.svg?branch=master)](https://travis-ci.org/dbaltas/nomes)
 # Nomes
 html to machine friendly format using xpath
 
 ## TODO
-* Read xpath expressions from config file
 * Convert results to json
 * input from kafka
 * handle xpath expressions returning lists [parser]
 * handle xpath expressions returning unwanted embedded html ex.`121,283 <span class="results-label">results</span>` [parser]
 
+## Run
+```
+$ cp config.yaml.example config.yaml
+$ go run main.go -in examples/so.htm
+```
 ## Test
 ```
 go test
